814e678b00884b83ec71eaed0f4804c958ea708e
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-01-26 21:00:46 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """
 helloworld.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: <Jasper Meyer>
+Credit: <none>
 
 Assignment:
 
@@ -10,3 +10,4 @@ Write and submit a Python program that prints the following:
 Hello, world!
 """
 
+print "Hello, world!"
